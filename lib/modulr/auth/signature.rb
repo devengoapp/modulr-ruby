408f8108eea67f875710ce83db5e800e46a78c2b
Aitor García Rey <aitor@linkingpaths.com>
2022-07-16 17:06:05 +0200

    feat(core): Request signature lib

---
+++ lib/modulr/auth/signature.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require "base64"
+require "date"
+require "openssl"
+require "securerandom"
+require "erb"
+
+module Modulr
+  module Auth
+    class Signature
+      attr_reader :nonce, :signature, :timestamp, :authorization
+
+      def initialize(apikey:, nonce:, signature:, timestamp:)
+        @nonce = nonce
+        @signature = signature
+        @timestamp = timestamp
+        @authorization = [
+          "Signature keyId=\"#{apikey}\"",
+          'algorithm="hmac-sha1"',
+          'headers="date x-mod-nonce""',
+          "signature=\"#{signature}\"",
+        ].join(",")
+      end
+
+      def self.calculate(apikey:, apisecret:, nonce: SecureRandom.base64(30), timestamp: DateTime.now.httpdate)
+        signature_string = "date: #{timestamp}\nx-mod-nonce: #{nonce}"
+        digest = OpenSSL::HMAC.digest(
+          "SHA1",
+          apisecret.force_encoding("UTF-8"),
+          signature_string.force_encoding("UTF-8")
+        )
+        b64 = Base64.encode64(digest)
+        url_safe_code = ERB::Util.url_encode(b64.strip)
+
+        new(apikey: apikey, nonce: nonce, signature: url_safe_code, timestamp: timestamp)
+      end
+    end
+  end
+end
