daad6d1e9f845bf740471e0238644ec06bacfe21
Fran Moya <fran@devengo.com>
2022-08-29 16:02:31 +0200

    Add Bad Request error spec on accounts

--- spec/modulr/api/accounts_service_spec.rb
+++ spec/modulr/api/accounts_service_spec.rb
@@ -4,58 +4,99 @@ RSpec.describe Modulr::API::AccountsService, :unit, type: :client do
   subject(:accounts) { described_class.new(initialize_client) }
 
   describe "accounts create" do
-    before do
-      stub_request(:post, %r{/customers/C0000001/accounts}).to_return(
-        read_http_response_fixture("accounts/create", "success")
-      )
+    context "when success" do
+      before do
+        stub_request(:post, %r{/customers/C0000001/accounts}).to_return(
+          read_http_response_fixture("accounts/create", "success")
+        )
+      end
+
+      let!(:created_account) do
+        accounts.create(
+          customer_id: "C0000001",
+          currency: "EUR",
+          product_code: "productCode",
+          options: { external_reference: "aReference_00001" }
+        )
+      end
+
+      it_behaves_like "builds correct request", {
+        method: :post,
+        path: %r{/customers/C0000001/accounts},
+        body: {
+          currency: "EUR",
+          productCode: "productCode",
+          externalReference: "aReference_00001",
+        },
+      }
+
+      it "returns created account" do
+        expect(created_account).to be_a Modulr::Resources::Accounts::Account
+        expect(created_account.customer_id).to eql("C0000001")
+        expect(created_account.external_reference).to eql("aReference_00001")
+        expect(created_account.balance).to eql("0.00")
+        expect(created_account.available_balance).to be_nil
+      end
     end
 
-    let!(:created_account) do
-      accounts.create(
-        customer_id: "C0000001",
-        currency: "EUR",
-        product_code: "productCode",
-        options: { external_reference: "aReference_00001" }
-      )
-    end
+    context "when fails" do
+      before do
+        stub_request(:post, %r{/customers/C0000001/accounts}).to_return(
+          read_http_response_fixture("accounts/create", "failure")
+        )
+      end
 
-    it_behaves_like "builds correct request", {
-      method: :post,
-      path: %r{/customers/C0000001/accounts},
-      body: {
-        currency: "EUR",
-        productCode: "productCode",
-        externalReference: "aReference_00001",
-      },
-    }
+      let(:failed_created_account) do
+        accounts.create(
+          customer_id: "C0000001",
+          currency: "EUR",
+          product_code: nil,
+          options: { external_reference: "aReference_00001" }
+        )
+      end
 
-    it "returns created account" do
-      expect(created_account).to be_a Modulr::Resources::Accounts::Account
-      expect(created_account.customer_id).to eql("C0000001")
-      expect(created_account.external_reference).to eql("aReference_00001")
-      expect(created_account.balance).to eql("0.00")
-      expect(created_account.available_balance).to be_nil
+      it "returns bad_request response" do
+        expect { failed_created_account }.to raise_error(Faraday::BadRequestError)
+      end
     end
   end
 
   describe "accounts close" do
-    before do
-      stub_request(:post, %r{/accounts/A121AHGZ/close}).to_return(
-        read_http_response_fixture("accounts/close", "success")
-      )
+    context "when success" do
+      before do
+        stub_request(:post, %r{/accounts/A121AHGZ/close}).to_return(
+          read_http_response_fixture("accounts/close", "success")
+        )
+      end
+
+      let!(:method_response) do
+        accounts.close(account_id: "A121AHGZ")
+      end
+
+      it_behaves_like "builds correct request", {
+        method: :post,
+        path: %r{/accounts/A121AHGZ/close},
+      }
+
+      it "returns nil" do
+        expect(method_response).to be_nil
+      end
     end
 
-    let!(:method_response) do
-      accounts.close(account_id: "A121AHGZ")
-    end
+    context "when fails" do
+      before do
+        stub_request(:post, %r{/accounts/unexisting_account_id/close}).to_return(
+          read_http_response_fixture("accounts/close", "failure")
+        )
+      end
 
-    it_behaves_like "builds correct request", {
-      method: :post,
-      path: %r{/accounts/A121AHGZ/close},
-    }
+      let(:failed_closed_account) do
+        accounts.close(account_id: "unexisting_account_id")
+      end
 
-    it "returns nil" do
-      expect(method_response).to be_nil
+      it "returns bad_request response" do
+        expect { failed_closed_account }.to raise_error(Faraday::BadRequestError)
+      end
     end
   end
 end
