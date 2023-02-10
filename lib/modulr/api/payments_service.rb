9a3d8a10ef15f6ba4a81a1f114720484d84f4ba2
Aitor García Rey <aitor@linkingpaths.com>
2023-02-10 12:12:25 +0100

    feat(payments): Extracting the filter parameters for payment listing

--- lib/modulr/api/payments_service.rb
+++ lib/modulr/api/payments_service.rb
@@ -12,19 +12,13 @@ module Modulr
       end
 
       def list(**opts)
-        return find(id: opts[:id] ) if opts[:id]
+        return find(id: opts[:id]) if opts[:id]
 
-        params = {}
-        params[:fromCreatedDate] = format_datetime(opts[:from]) if opts[:from]
-        params[:toCreatedDate] = format_datetime(opts[:to]) if opts[:to]
-        params[:type] = opts[:type] if opts[:type]
-        params[:externalReference] = opts[:external_reference] if opts[:external_reference]
-        params[:status] = opts[:status] if opts[:status]
-        params[:sourceAccountId] = opts[:account_id] if opts[:account_id]
-
-        _response = client.get("/payments", params)
+        response = client.get("/payments", build_query_params(opts))
+        Resources::Payments::Payments.new(response, response.body[:content])
       end
 
+      # rubocop:disable Metrics/ParameterLists
       def create(account_id:, destination:, reference:, currency:, amount:, **opts)
         payload = {
           sourceAccountId: account_id,
@@ -34,9 +28,30 @@ module Modulr
           amount: amount,
         }
 
+        payload[:externalReference] = opts[:external_reference] if opts[:external_reference]
+        payload[:endToEndReference] = opts[:e2e_reference] if opts[:e2e_reference]
+
         response = client.post("/payments", payload)
         Resources::Payments::Payment.new(response, response.body)
       end
+      # rubocop:enable Metrics/ParameterLists
+
+      # rubocop:disable Metrics/AbcSize
+      private def build_query_params(opts)
+        same_name_params = [:type, :status]
+        date_params = { to: :toCreatedDate, from: :fromCreatedDate, updated_since: :modifiedSince }
+        mapped_params = {
+          external_reference: :externalReference,
+          has_external_reference: :hasExternalReference,
+          account_id: :sourceAccountId,
+        }
+        {}.tap do |params|
+          same_name_params.each { |param| params[param] = opts[param] if opts[param] }
+          date_params.each { |original, mapped| params[mapped] = format_datetime(opts[original]) if opts[original] }
+          mapped_params.each { |original, mapped| params[mapped] = opts[original] if opts[original] }
+        end
+      end
+      # rubocop:enable Metrics/AbcSize
     end
   end
 end
