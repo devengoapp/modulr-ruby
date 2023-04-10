4dc7b4150a981cbda2c129a7b0f4ed5af5bd7b02
carloslopdev <carlos@devengo.com>
2023-04-10 18:30:01 +0200

    Method to create customer

--- lib/modulr/api/customers_service.rb
+++ lib/modulr/api/customers_service.rb
@@ -7,6 +7,30 @@ module Modulr
         response = client.get("/customers/#{id}")
         Resources::Customers::Customer.new(response.env[:raw_body], response.body)
       end
+
+      def create(type:, legal_entity:, **opts)
+        payload = {
+          type: type,
+          legalEntity: legal_entity,
+        }
+
+        payload[:externalReference] = opts[:external_reference] if opts[:external_reference]
+        payload[:name] = opts[:name] if opts[:name]
+        payload[:companyRegNumber] = opts[:company_reg_number] if opts[:company_reg_number]
+        payload[:registeredAddress] = opts[:registered_address] if opts[:registered_address]
+        payload[:tradingAddress] = opts[:trading_address] if opts[:trading_address]
+        payload[:industryCode] = opts[:industry_code] if opts[:industry_code]
+        payload[:tcsVersion] = opts[:tcs_version] if opts[:tcs_version]
+        payload[:expectedMonthlySpend] = opts[:expected_monthly_spend] if opts[:expected_monthly_spend]
+        payload[:associates] = opts[:associates] if opts[:associates]
+        payload[:documentInfo] = opts[:document_info] if opts[:document_info]
+        payload[:provisionalCustomerId] = opts[:provisional_customer_id] if opts[:provisional_customer_id]
+        payload[:customerTrust] = opts[:customer_trust] if opts[:customer_trust]
+        payload[:taxProfile] = opts[:tax_profile] if opts[:tax_profile]
+
+        response = client.post("/customers", payload)
+        Resources::Customers::Customer.new(response.env[:raw_body], response.body)
+      end
     end
   end
 end
